7e9096783f517f43c21a8efeb794f730d343dc66
Sonam Thapa <sonamthapa@codemantra.in>
2024-09-14 15:43:50 +0545

    init commit

---
+++ client/src/components/CustomColumnMenu.jsx
@@ -0,0 +1,29 @@
+// import {
+//     GridColumnMenuContainer,
+//     GridFilterMenuItem,
+//     HideGridColMenuItem,
+//   } from "@mui/x-data-grid";
+
+//   const CustomColumnMenu = (props) => {
+//     const { hideMenu, currentColumn, open } = props;
+//     return (
+//       <GridColumnMenuContainer
+//         hideMenu={hideMenu}
+//         currentColumn={currentColumn}
+//         open={open}
+//       >
+//         <GridFilterMenuItem onClick={hideMenu} column={currentColumn} />
+//         <HideGridColMenuItem onClick={hideMenu} column={currentColumn} />
+//       </GridColumnMenuContainer>
+//     );
+//   };
+
+//   export default CustomColumnMenu;
+
+import React from 'react';
+
+const CustomColumnMenu = () => {
+  return <div>CustomColumnMenu</div>;
+};
+
+export default CustomColumnMenu;
